1cb5417fab85273135c46524cb462cac39ac44c3
pst <ruslan.ermeichook@gmail.com>
2017-03-12 12:51:29 +0300

    LessonTwo

---
+++ WinForms/LessonTwo/MainForm.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace LessonTwo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBoxCities = new System.Windows.Forms.ListBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.hScrollBarR = new System.Windows.Forms.HScrollBar();
+            this.progressBarMain = new System.Windows.Forms.ProgressBar();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.toolTipMain = new System.Windows.Forms.ToolTip(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabelCities = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.hScrollBarBlue = new System.Windows.Forms.HScrollBar();
+            this.hScrollBarGreen = new System.Windows.Forms.HScrollBar();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBoxCities
+            // 
+            this.listBoxCities.DisplayMember = "Name";
+            this.listBoxCities.FormattingEnabled = true;
+            this.listBoxCities.Location = new System.Drawing.Point(12, 12);
+            this.listBoxCities.Name = "listBoxCities";
+            this.listBoxCities.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBoxCities.Size = new System.Drawing.Size(120, 147);
+            this.listBoxCities.TabIndex = 0;
+            this.listBoxCities.SelectedIndexChanged += new System.EventHandler(this.listBoxCities_SelectedIndexChanged);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Tourism",
+            "Work"});
+            this.checkedListBox1.Location = new System.Drawing.Point(138, 12);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(139, 94);
+            this.checkedListBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Free",
+            "Chargeable"});
+            this.comboBox1.Location = new System.Drawing.Point(283, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(728, 12);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(17, 542);
+            this.vScrollBar1.TabIndex = 3;
+            // 
+            // hScrollBarR
+            // 
+            this.hScrollBarR.Location = new System.Drawing.Point(9, 507);
+            this.hScrollBarR.Name = "hScrollBarR";
+            this.hScrollBarR.Size = new System.Drawing.Size(736, 17);
+            this.hScrollBarR.TabIndex = 4;
+            this.hScrollBarR.ValueChanged += new System.EventHandler(this.hScrollBarForProgress_ValueChanged);
+            // 
+            // progressBarMain
+            // 
+            this.progressBarMain.Location = new System.Drawing.Point(12, 205);
+            this.progressBarMain.Name = "progressBarMain";
+            this.progressBarMain.Size = new System.Drawing.Size(713, 23);
+            this.progressBarMain.TabIndex = 5;
+            this.toolTipMain.SetToolTip(this.progressBarMain, "Progress Bar ToolTip");
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(422, 13);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 6;
+            // 
+            // toolTipMain
+            // 
+            this.toolTipMain.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.toolTipMain.ToolTipTitle = "Main tooltips";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelCities,
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 558);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(754, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabelCities
+            // 
+            this.toolStripStatusLabelCities.Name = "toolStripStatusLabelCities";
+            this.toolStripStatusLabelCities.Size = new System.Drawing.Size(0, 17);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(98, 17);
+            this.toolStripStatusLabel1.Text = "Инфо о поездке:";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // hScrollBarBlue
+            // 
+            this.hScrollBarBlue.Location = new System.Drawing.Point(9, 438);
+            this.hScrollBarBlue.Name = "hScrollBarBlue";
+            this.hScrollBarBlue.Size = new System.Drawing.Size(736, 17);
+            this.hScrollBarBlue.TabIndex = 4;
+            this.hScrollBarBlue.ValueChanged += new System.EventHandler(this.hScrollBarForProgress_ValueChanged);
+            // 
+            // hScrollBarGreen
+            // 
+            this.hScrollBarGreen.Location = new System.Drawing.Point(9, 470);
+            this.hScrollBarGreen.Name = "hScrollBarGreen";
+            this.hScrollBarGreen.Size = new System.Drawing.Size(736, 17);
+            this.hScrollBarGreen.TabIndex = 4;
+            this.hScrollBarGreen.ValueChanged += new System.EventHandler(this.hScrollBarForProgress_ValueChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 580);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.progressBarMain);
+            this.Controls.Add(this.hScrollBarGreen);
+            this.Controls.Add(this.hScrollBarBlue);
+            this.Controls.Add(this.hScrollBarR);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.listBoxCities);
+            this.Name = "MainForm";
+            this.Text = "Main form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxCities;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.HScrollBar hScrollBarR;
+        private System.Windows.Forms.ProgressBar progressBarMain;
+        private System.Windows.Forms.ToolTip toolTipMain;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelCities;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.HScrollBar hScrollBarBlue;
+        private System.Windows.Forms.HScrollBar hScrollBarGreen;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ WinForms/LessonTwo/MainForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LessonTwo
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBoxCities.Items.Add(new City() {Id = 1, Name="Minsk" });
+            listBoxCities.Items.Add(new City() { Id = 1, Name = "Brest" });
+            listBoxCities.Items.Add(new City() { Id = 1, Name = "Gomel" });
+
+            var worker = new BackgroundWorker();
+            worker.DoWork += worker_DoWork;
+            worker.RunWorkerAsync();
+        }
+
+        void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (var i = 1; i < 11; i++)
+            {
+                this.Invoke((MethodInvoker)delegate { 
+                progressBarMain.Value += 10;
+                });
+               Thread.Sleep(1000);
+            }
+ 
+            //MessageBox.Show("Hello from another thread!");
+        }
+        private void listBoxCities_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            City.CalcBalance();
+            var list = new List<string>();
+            foreach (City i in listBoxCities.SelectedItems)
+            {
+                list.Add(i.Name);
+            }
+            toolStripStatusLabelCities.Text = string.Format("Мы едим в {0}", string.Join(",", list));
+        }
+
+        private void hScrollBarForProgress_ValueChanged(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(hScrollBarR.Value,
+                hScrollBarGreen.Value, hScrollBarBlue.Value);
+            City.CalcBalance();
+        }
+    }
+
+    public class City
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+
+        public static void CalcBalance(){}
+    }
+}
